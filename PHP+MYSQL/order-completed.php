12e1b909754646ffd228ee453a5d45e4b707ec27
DerekWong306 <88536461+forever2417@users.noreply.github.com>
2023-03-12 23:54:54 +0800

    Delete PHP+MYSQL directory

--- PHP+MYSQL/order-completed.php
+++
@@ -1,7 +0,0 @@
-<?php include_once('header.php'); ?>
-    <h1>Thanks for you purchase！</h1>
-    <div class="container">
-        <p class="thankyouText">Your order request is well-received!</p>
-    </div>
-
-<?php include_once('footer.php'); ?>
--- PHP+MYSQL/password.php
+++
@@ -1,9 +0,0 @@
-<?php 
-echo password_hash("abcabc",PASSWORD_BCRYPT);
-$hashedpw = password_hash("abcabc",PASSWORD_BCRYPT);
-
-if (password_verify("password123",$hashedpw)){
-    echo '<br> password is valid';
-} else {
-    echo '<br> password is invalid';
-}
