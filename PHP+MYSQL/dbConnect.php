a4c8b9c1cd87359006652ea8fda7197b1ed79086
DerekWong306 <88536461+forever2417@users.noreply.github.com>
2023-03-15 00:24:45 +0800

    Delete PHP+MYSQL directory

--- PHP+MYSQL/dbConnect.php
+++
@@ -1,8 +0,0 @@
-<?php
-$dbConnection = mysqli_connect("localhost","root","Aa95849685","php_gem");
-
-//check the connection success or not
-if (mysqli_connect_error()){
-    echo "Failed to connect to MYSQL: ".mysqli_connect_error();
-    exit();
-}
